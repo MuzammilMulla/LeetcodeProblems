6fc64dd05b65fb96363193db7720e97df9160958
MuzammilMulla <muzammilmulla999@gmail.com>
2022-08-30 20:01:43 +0530

    Fibonacci Number

---
+++ src/leetcodesolving/FibonacciNumber.java
@@ -0,0 +1,20 @@
+package leetcodesolving;
+
+public class FibonacciNumber {
+    public static void main(String[] args) {
+        int n= 10;
+        System.out.println(fib(n));
+    }
+        static int fib(int n) {
+
+            if(n==0){
+                return 0;
+            }
+            if(n==1){
+                return 1;
+            }
+            return fib(n-1) + fib(n-2);
+        }
+    }
+
+
